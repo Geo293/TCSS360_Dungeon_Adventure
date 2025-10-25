c61c45ce247c86c6973d98c7f395d6d675facf33
Geovani Vasquez <geovav@uw.edu>
2025-10-24 21:29:45 -0700

    I made a simple window

--- src/view/DungeonGUI.java
+++ src/view/DungeonGUI.java
@@ -1,13 +1,34 @@
-package view;
+package view; // only include this if your file is inside a "view" package
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 /**
  * Displays the dungeon
  * @author Geovani Vasquez
- * @version Oct, 24 2025
+ * @version Oct 24, 2025
  */
-public class DungeonGUI extends javax.swing.JFrame {
-    public DungeonGUI() {
+public class DungeonGUI extends Application {
 
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Dungeon_Adventure");
+        button = new Button("Start the game");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene openingWindow = new Scene(layout, 300, 250);
+        primaryStage.setScene(openingWindow);
+        primaryStage.show();
+    }
 }
