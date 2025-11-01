46e4e008a55208791724b53fa100a58d91149a23
justi <justinyee143s@gmail.com>
2025-11-01 16:53:31 -0700

    Done with simple methods (place exits and stuff)

--- src/model/Dungeon.java
+++ src/model/Dungeon.java
@@ -1,6 +1,8 @@
 package model;
 
 import java.util.Random;
+import java.util.Stack;
+
 /**
  * This class creates a randomly generated maze for the player to
  * travel through the maze will always have an  entrance, an exit, and
@@ -44,23 +46,76 @@ public class Dungeon {
     public Dungeon (int width, int height){
         this.width = width;
         this.height = height;
-        this.maze = new Room[height][width];
+        this.maze = new Room[width][height];
         this.random = new Random();
 
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
                 maze[i][j] = new Room();
             }
         }
 
         generateMaze();
+        placeExit();
+        placeEntrance();
+        placePillars();
     }
 
-
-    public void generateMaze(){
+    /**
+     * We are using  the Depth-First-Search (DFS) algorithm to generate a maze for the Dungeon
+     * Places entrance, exit, and pillars down as well.
+     */
+    private void generateMaze(){
 
     }
 
+    /**
+     * Places the entrance at 0,0 and sets the hero x,y to 0,0 as well.
+     */
+    private void placeEntrance(){
+        entranceX = 0;
+        entranceY = 0;
+        maze[entranceX][entranceY].setEntrance(true);
+        heroX = entranceX;
+        heroY = entranceY;
+    }
 
+    /**
+     * Places the exit in the bottom left corner of the dungeon.
+     */
+    private void placeExit(){
+        exitX = width - 1;
+        exitY = height - 1;
+        maze[exitX][exitY].setExit(true);
+    }
+
+    /**
+     * Places all the pillars in random locations in the dungeon.
+     */
+    private void placePillars(){
+        for (String pillar : PILLAR_TYPES) {
+            int pillarX, pillarY;
+            do {
+                pillarX = random.nextInt(width);
+                pillarY = random.nextInt(height);
+            } while ((pillarX == entranceX && pillarY == entranceY) ||
+                    (pillarX == exitX && pillarY == exitY) ||
+                    maze[pillarX][pillarY].getPillar() != null);
+
+            maze[pillarX][pillarY].setPillar(pillar);
+        }
+    }
+
+    /**
+     * Generates all the contents in the Rooms of the Dungeon.
+     */
+    private void generateRoomContents(){
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                if (!maze[x][y].isEntrance() && !maze[x][y].isExit()) {
+                    maze[x][y].generateContents();
+                }
+            }
+        }
+    }
 }
-
