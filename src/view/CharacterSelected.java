c5fc38beb5ea49b3ad970d848d05907bab95ed4a
Geovani Vasquez <geovav@uw.edu>
2025-10-31 20:54:47 -0700

    I added to the character select class to where you can choose a character and then are prompted a confirm options then are allowed to pick your name and when you go back to main menu it resets everything to how it was originally.

--- src/view/CharacterSelected.java
+++ src/view/CharacterSelected.java
@@ -3,23 +3,155 @@ package view;
 import controller.GameController;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.TextField;
 
+/**
+ * this class displays a character selection screen that will prompted
+ * the user to choose what character they want and displays their stats,
+ * and let the user name their character.
+ *
+ * @author Geovani Vasquez
+ * @version Oct, 31 2025
+ */
 public class CharacterSelected extends Scene {
     private Button myBackButton;
     private TextField myCharacterName;
     private GameController myController;
+    private Button myThief;
+    private Button myPriestess;
+    private Button myWarrior;
+    private Label myStatsDisplay;
+    private Button myConfirmChar;
 
-
+    /**
+     * This is the main method that sets the instance fields
+     * and set the design of the character select screen.
+     * @param theController tells the window what screen to display.
+     */
     public CharacterSelected(GameController theController) {
         super(new VBox(),300,250);
+        myBackButton = new Button("Back to main menu");
+        myStatsDisplay = new Label("Select a Character");
         myController = theController;
         myCharacterName = new TextField();
-        myBackButton = new Button("Back to main menu");
-        myBackButton.setOnAction(e -> myController.goBackToStart());
+        myCharacterName.setVisible(false);
+        VBox characters = createCharacterButtons();
+        HBox characterWStats = showStats(characters);
+        HBox bottomSec = bacKToMenu();
+        ((VBox)this.getRoot()).getChildren().addAll(characterWStats, bottomSec);
 
-        ((VBox)this.getRoot()).getChildren().addAll(myCharacterName, myBackButton);
+    }
+
+    /**
+     * This displays the options for characters such as warrior priestess
+     * and thief then prompts the user to confirm their option.
+     * @return the window that contains the characters.
+     */
+    public VBox createCharacterButtons(){
+        myWarrior = new Button("Warrior");
+        myPriestess = new Button("Priestess");
+        myThief = new Button("Thief");
+        myWarrior.setOnAction(e -> {
+            showStats("Warrior");
+            myConfirmChar.setVisible(true);
+            myConfirmChar.requestFocus();
+            });
+        myPriestess.setOnAction(e -> {
+            showStats("Priestess");
+            myConfirmChar.setVisible(true);
+            myConfirmChar.requestFocus();
+            });
+        myThief.setOnAction(e -> {
+            showStats("Thief");
+            myConfirmChar.setVisible(true);
+            myConfirmChar.requestFocus();
+        });
+        VBox characterButtons = new VBox();
+        characterButtons.getChildren().addAll(myWarrior, myPriestess, myThief);
+        return characterButtons;
+
+    }
+
+    /**
+     * This sets the options to confirm a character or return to the main
+     * menu and when the character is selected you are then allowed to name it.
+     *
+     * @return the bottom window.
+     */
+    public HBox bacKToMenu(){
+        HBox bac = new HBox();
+        myConfirmChar = new Button("confirm");
+        myConfirmChar.setVisible(false);
+        myConfirmChar.setOnAction(e -> {
+            myCharacterName.setVisible(true);
+            myCharacterName.requestFocus();
+        });
+        myBackButton.setOnAction(e -> {
+            myController.goBackToStart();
+            myStatsDisplay.setText("Select a Character");
+            myCharacterName.setVisible(false);
+            myCharacterName.clear();
+            myConfirmChar.setVisible(false);
+        });
+        bac.getChildren().addAll(myBackButton, myConfirmChar, myCharacterName);
+        return bac;
+    }
+
+    /**
+     * This sets u[ the character buttons and their stats to be side by side
+     * @param theCharButton the display for the character buttons
+     * @return the character stat display
+     */
+    public HBox showStats(VBox theCharButton){
+        HBox statsDisplay = new HBox();
+        statsDisplay.getChildren().addAll(theCharButton, myStatsDisplay);
+        return statsDisplay;
+
+
+    }
+
+    /**
+     * This is the fromate for all the stats so when showstats is called
+     * it displays the corresponding stat of the character being called.
+     * @param theName The character being choosen
+     */
+    public void showStats(String theName) {
+        String stats = "";
+
+        switch(theName) {
+            case "Warrior":
+                stats = "Hit Points: 125\n" +
+                        "Attack Speed: 4\n" +
+                        "Chance to Hit: 80%\n" +
+                        "Min Damage: 35\n" +
+                        "Max Damage: 60\n" +
+                        "Block Chance: 20%\n" +
+                        "Special: Crushing Blow";
+                break;
+            case "Priestess":
+                stats = "Hit Points: 75\n" +
+                        "Attack Speed: 5\n" +
+                        "Chance to Hit: 70%\n" +
+                        "Min Damage: 25\n" +
+                        "Max Damage: 45\n" +
+                        "Block Chance: 30%\n" +
+                        "Special: HEAL";
+                break;
+            case "Thief":
+                stats = "Hit Points: 75\n" +
+                        "Attack Speed: 6\n" +
+                        "Chance to Hit: 80%\n" +
+                        "Min Damage: 20\n" +
+                        "Max Damage: 40\n" +
+                        "Block Chance: 40%\n" +
+                        "Special: surprise attack";
+                break;
+        }
+
+        myStatsDisplay.setText(stats);
 
     }
 
