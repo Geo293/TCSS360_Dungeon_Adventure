7a672eb681370285fac8de7c2565714f5ed3aacc
Geovani Vasquez <geovav@uw.edu>
2025-11-01 19:54:06 -0700

    i added what carson has putt to my iterration so that it works better with what we have so far

--- src/model/DungeonCharacter.java
+++ src/model/DungeonCharacter.java
@@ -3,68 +3,75 @@ package model;
 import java.util.Random;
 
 /**
- * this class the parent class that every monster and or character will
- * be based on which include how many hit points the npc will have ,
- * its name, attack speed, range, and attack behavior
- *@author Geovani Vasquez
- * @version Oct, 24 2025
+ * Abstract base class for all dungeon characters (Heroes and Monsters).
+ * Encapsulates shared attributes/behaviors such as attacking, hit points, and  attack speed.
  */
-public class DungeonCharacter {
-    protected String myName;
-    protected int myHealth;
-    protected int myMaxHealth;
-    protected int myDamageMin;
-    protected int myDamageMax;
-    protected int myAttackSpeed;
-    protected double myChance;
+public abstract class DungeonCharacter {
+    protected String name;
+    protected int hitPoints;
+    protected int minDamage;
+    protected int maxDamage;
+    protected int attackSpeed;
+    protected double chanceToHit;
 
-    protected DungeonCharacter(String theName, int theHitPoints, int theMinDamage,
-                               int theMaxDamage, int theAttackSpeed, double theChance) {
-        myName = theName;
-        myHealth = theHitPoints;
-        myMaxHealth = theHitPoints;
-        myDamageMin = theMinDamage;
-        myDamageMax = theMaxDamage;
-        myAttackSpeed = theAttackSpeed;
-        myChance = theChance;
-    }
-    public void attack(DungeonCharacter opponent) {
-        Random rand = new Random();
-        double roll = rand.nextDouble();
-        int damageRoll = rand.nextInt(myDamageMax - myDamageMin) + myDamageMin +1;
-        if(roll <= myChance){
-            opponent.myHealth -= damageRoll;
-        } else {
-            System.out.print("faild");
-        }
-    }
-    public String getName() {
-        return myName;
-    }
-    public int getHealth() {
-        return myHealth;
-    }
-    public int getMyAttackSpeed(){
-        return myAttackSpeed;
-    }
-    public boolean isAlive(){
-        return myHealth > 0;
-    }
+    protected Random rand;
 
     /**
-     * Whenever the user gets healed it will call this
-     * method which will determined if the user health is
-     * full and then raise its health depending on that.
-     * @param theHeal
+     * Protected constructor to be called by subclasses(Hero Monster,etc).
      */
-    public void setHealth(int theHeal) {
-        if (myHealth < myMaxHealth) {
-            myHealth = theHeal + myHealth;
-            if (myHealth > myMaxHealth) {
-                myHealth = myMaxHealth;
+    protected DungeonCharacter(String name, int hitPoints, int minDamage, int maxDamage,
+                               int attackSpeed, double chanceToHit) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.minDamage = minDamage;
+        this.maxDamage = maxDamage;
+        this.attackSpeed = attackSpeed;
+        this.chanceToHit = chanceToHit;
+        this.rand = new Random();
+    }
+
+    // Getters and setters
+    public String getName() { return name; }
+    public int getHitPoints() { return hitPoints; }
+    public int getAttackSpeed() { return attackSpeed; }
+    public void setHitPoints(int hp) { this.hitPoints = Math.max(hp, 0); }
+    public boolean isAlive() { return hitPoints > 0; }
+
+    /**
+     * Attack another DungeonCharacter(hero or Monster)
+     * Number of attacks is determined by comparing attack speeds.
+     */
+    public void attack(DungeonCharacter opponent) {
+        int numAttacks = Math.max(1, this.attackSpeed / opponent.attackSpeed);
+
+        for (int i = 0; i < numAttacks; i++) {
+            if (rand.nextDouble() <= chanceToHit) {
+                int damage = rand.nextInt(maxDamage - minDamage + 1) + minDamage;
+                opponent.subtractHitPoints(damage);
+                System.out.println(name + " hits " + opponent.name + " for " + damage + " damage.");
+            } else {
+                System.out.println(name + " missed the attack on " + opponent.name + ".");
+            }
+
+            if (!opponent.isAlive()) {
+                System.out.println(opponent.name + " has been defeated!");
+                break;
             }
         }
     }
+
+    /**
+     * Subtract hit points from the character.
+     */
+    public void subtractHitPoints(int damage) {
+        setHitPoints(hitPoints - damage);
+    }
+
+    /**
+     * Display character stats.
+     */
+    public String toString() {
+        return String.format("%s [HP: %d, Speed: %d, Damage: %d-%d, Hit Chance: %.2f]",
+                name, hitPoints, attackSpeed, minDamage, maxDamage, chanceToHit);
+    }
 }
-
-
